1e326f5043da68ca719ae122ecbef28130f36255
Lucas Lourenço Miranda <lucas.adereco@gmail.com>
2022-12-08 05:54:15 -0300

    Add SearchPage.js

---
+++ starter/src/SearchPage.js
@@ -0,0 +1,28 @@
+
+
+const SearchPage = () => {
+
+    return (
+        <div className="search-books">
+            <div className="search-books-bar">
+                <a
+                    className="close-search"
+                    onClick={() => setShowSearchpage(!showSearchPage)}
+                >
+                    Close
+                </a>
+                <div className="search-books-input-wrapper">
+                    <input
+                        type="text"
+                        placeholder="Search by title, author, or ISBN"
+                    />
+                </div>
+            </div>
+            <div className="search-books-results">
+                <ol className="books-grid"></ol>
+            </div>
+        </div>
+    );
+};
+
+export default SearchPage;
