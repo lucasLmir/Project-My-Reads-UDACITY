bef82bcc5cfee7bc970996e218fa3061f6842eb3
Lucas Lourenço Miranda <lucas.adereco@gmail.com>
2022-12-08 05:46:41 -0300

    Update MyReads.js

--- starter/src/MyReads.js
+++ starter/src/MyReads.js
@@ -1,5 +1,8 @@
+import "./App.css";
+import { useState } from "react";
 
 const MyReads = () => {
+    const [showSearchPage, setShowSearchpage] = useState(false);
 
     return (
         <div className="list-books">
