366003225768f4918327fac59e98130911311203
Lucas Lourenço Miranda <lucas.adereco@gmail.com>
2022-12-11 04:25:34 -0300

    refactor: Create shelfs array

--- starter/src/BookShelf.js
+++ starter/src/BookShelf.js
@@ -2,6 +2,21 @@ import "./App.css";
 import BookGrid from "./BookGrid";
 
 const BookShelf = ({ BookShelfTitle }) => {
+    const shelf = [
+        {
+            shelf: "currentlyReading",
+            title: "Currently Reading"
+        },
+        {
+            shelf: "wantToRead",
+            title: "Want to Read"
+        },
+        {
+            shelf: "read",
+            title: "Read"
+        },
+    ];
+
     return (
         <div className="bookshelf">
             <h2 className="bookshelf-title">{BookShelfTitle}</h2>
